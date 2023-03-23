9cf8943ecd2381f2ddb20378f1a6dfe41ebb68af
elvin <12527067+elvinlari@users.noreply.github.com>
2023-03-23 16:33:54 +0300

    first commit

---
+++ src/Token01.hs
@@ -0,0 +1,107 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE DeriveAnyClass      #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
+
+module Token01 where
+
+import           Control.Monad          hiding (fmap)
+import           Data.Aeson             (ToJSON, FromJSON)
+import           Data.Default           (Default (..))
+import           Data.Text              (Text)
+import           Data.Void              (Void)
+import           GHC.Generics           (Generic)
+import           Plutus.Contract        as Contract
+import           Plutus.Trace.Emulator  as Emulator
+import qualified PlutusTx
+import           PlutusTx.Prelude       hiding (Semigroup(..), unless)
+import           Ledger                 hiding (mint, singleton)
+import           Ledger.Constraints     as Constraints
+import           Ledger.TimeSlot
+import qualified Ledger.Typed.Scripts   as Scripts
+import           Ledger.Value           as Value
+import           Playground.Contract    (printJson, printSchemas, ensureKnownCurrencies, stage, ToSchema)
+import           Playground.TH          (mkKnownCurrencies, mkSchemaDefinitions)
+import           Playground.Types       (KnownCurrency (..))
+import           Prelude                (IO, Semigroup (..), Show (..), String)
+import           Text.Printf            (printf)
+import           Wallet.Emulator.Wallet
+
+{-# INLINABLE mkPolicy #-}
+-- This policy should only allow minting (or burning) of tokens if the owner of the specified PaymentPubKeyHash
+-- has signed the transaction.
+mkPolicy :: PaymentPubKeyHash -> () -> ScriptContext -> Bool
+mkPolicy pkh () ctx = txSignedBy (scriptContextTxInfo ctx) $ unPaymentPubKeyHash pkh
+
+policy :: PaymentPubKeyHash -> Scripts.MintingPolicy
+policy pkh = mkMintingPolicyScript $
+    $$(PlutusTx.compile [|| Scripts.wrapMintingPolicy . mkPolicy ||])
+    `PlutusTx.applyCode`
+    PlutusTx.liftCode pkh
+
+
+
+
+curSymbol :: PaymentPubKeyHash -> CurrencySymbol
+curSymbol = scriptCurrencySymbol . policy
+
+data MintParams = MintParams
+    { mpTokenName :: !TokenName
+    , mpAmount    :: !Integer
+    } deriving (Generic, ToJSON, FromJSON, ToSchema)
+
+type SignedSchema = Endpoint "mint" MintParams
+
+mint :: MintParams -> Contract w SignedSchema Text ()
+mint mp = do
+    pkh <- Contract.ownPaymentPubKeyHash
+    let val     = Value.singleton (curSymbol pkh) (mpTokenName mp) (mpAmount mp)
+        lookups = Constraints.mintingPolicy $ policy pkh
+        tx      = Constraints.mustMintValue val 
+    ledgerTx <- submitTxConstraintsWith @Void lookups tx
+    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+    Contract.logInfo @String $ printf "forged %s" (show val)
+            
+
+endpoints :: Contract () SignedSchema Text ()
+endpoints = mint' >> endpoints
+  where
+    mint' = awaitPromise $ endpoint @"mint" mint
+
+mkSchemaDefinitions ''SignedSchema
+
+mkKnownCurrencies []
+
+-- For minting policy:
+writeMintingPolicy :: Plutus.MintingPolicy -> IO (Either (FileError ()) ())
+writeMintingPolicy  = writeFileTextEnvelope @(PlutusScript PlutusScriptV1) "my-minting-script.plutus" Nothing . PlutusScriptSerialised . SBS.toShort . LBS.toStrict . serialise . Plutus.getMintingPolicy
+
+
+-- writeUnit :: IO ()
+-- writeUnit = writeJSON "scripts/unit.json" ()
+
+-- -- For validator:
+-- writeValidator :: Plutus.Validator -> IO (Either (FileError ()) ())
+-- writeValidator = writeValidator "scripts/demoContract.plutus" . validator
+
+-- test :: IO ()
+-- test = runEmulatorTraceIO $ do
+--     let tn       = "ABC"
+--     h <- activateContractWallet (knownWallet 1) endpoints
+--     callEndpoint @"mint" h $ MintParams
+--         { mpTokenName = tn
+--         , mpAmount    = 555
+--         }
+--     void $ Emulator.waitNSlots 110
+--     callEndpoint @"mint" h $ MintParams
+--         { mpTokenName = tn
+--         , mpAmount    = 555
+--         }
+--     void $ Emulator.waitNSlots 1
